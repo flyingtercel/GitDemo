0ca7e6409644bd618c71455e23ea5d11c9a2a636
黑夜之火 <flyingter@163.com>
2017-12-08 08:47:07 +0800

    这是第一次修改了一些信息

--- app/src/main/java/us/mifeng/gitdemo/MainActivity.java
+++ app/src/main/java/us/mifeng/gitdemo/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这是我的第一次修改
     }
 }
